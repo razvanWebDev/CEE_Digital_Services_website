a2513bb1a5f243976ebd7e64dc64a86348e89f58
razvanWebDev <crsn_razvan@yahoo.com>
2021-02-16 18:34:12 +0200

    fix video on partnership page

--- admin/includes/view_partnership_options_sponsorship_example.php
+++ admin/includes/view_partnership_options_sponsorship_example.php
@@ -21,7 +21,11 @@
             echo "<td>{$content}</td>";
             echo "<td>";
                 echo "<div class='table-iframe-container'>";
-                echo "<iframe width=\"420\" height=\"315\" src=\"http://www.youtube.com/embed/$video\" frameborder=\"0\" allowfullscreen></iframe>";
+                ?>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $video;?>" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+                <?php
                 echo "</div>";
              echo "</td>";
             echo "</tr>";
--- partnership-options.php
+++ partnership-options.php
@@ -56,8 +56,12 @@
                             echo "<div class='text-div'>{$content}</div>";
                             echo "<div class='media-div'>";
                                 echo "<div class='iframe-container'>";
-                                    echo "<iframe width=\"420\" height=\"315\" src=\"http://www.youtube.com/embed/$video\" frameborder=\"0\" allowfullscreen></iframe>";
-                                echo "</div>";
+                                ?>
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $video;?>" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                                <?php
+                                    echo "</div>";
                              echo "</div>";
                         echo "</div>";
                     }else{
